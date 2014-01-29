274678247a79f950d6b188c8be468c25b82fa29f
Manuel <manuel@allaboutapps.at>
2014-01-29 18:05:00 +0100

    fixed gauge

--- android/src/com/eiabea/btcdroid/fragments/PayoutFragment.java
+++ android/src/com/eiabea/btcdroid/fragments/PayoutFragment.java
@@ -11,6 +11,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -24,7 +26,7 @@ public class PayoutFragment extends Fragment {
 
 	public static final String PARAM_PRICE = "param_price";
 	public static final String PARAM_PROFILE = "param_profile";
-	
+
 	private GenericPrice price;
 	private Profile profile;
 
@@ -36,11 +38,12 @@ public class PayoutFragment extends Fragment {
 	private SharedPreferences pref;
 	private LinearLayout llPriceHolder;
 
-	public View onCreateView(LayoutInflater inflater, ViewGroup root, Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup root,
+			Bundle savedInstanceState) {
 		this.rootView = inflater.inflate(R.layout.fragment_payout, root, false);
 
 		pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
-		
+
 		price = getArguments().getParcelable(PARAM_PRICE);
 		profile = getArguments().getParcelable(PARAM_PROFILE);
 
@@ -61,15 +64,21 @@ public class PayoutFragment extends Fragment {
 	}
 
 	private void initUi() {
-		llPriceHolder = (LinearLayout) rootView.findViewById(R.id.ll_main_info_price_holder);
-		txtCurrentValue = (TextView) rootView.findViewById(R.id.txt_main_info_current_value);
+		llPriceHolder = (LinearLayout) rootView
+				.findViewById(R.id.ll_main_info_price_holder);
+		txtCurrentValue = (TextView) rootView
+				.findViewById(R.id.txt_main_info_current_value);
 
-		txtEstimatedReward = (TextView) rootView.findViewById(R.id.txt_main_info_estimated_reward);
-		txtConfirmedReward = (TextView) rootView.findViewById(R.id.txt_main_info_confirmed_reward);
-		txtTotalReward = (TextView) rootView.findViewById(R.id.txt_main_info_total_reward);
+		txtEstimatedReward = (TextView) rootView
+				.findViewById(R.id.txt_main_info_estimated_reward);
+		txtConfirmedReward = (TextView) rootView
+				.findViewById(R.id.txt_main_info_confirmed_reward);
+		txtTotalReward = (TextView) rootView
+				.findViewById(R.id.txt_main_info_total_reward);
 
 		prgGauge = (ProgressBar) rootView.findViewById(R.id.prg_data_gauge);
-		txtSendThreshold = (TextView) rootView.findViewById(R.id.txt_main_info_send_threshold);
+		txtSendThreshold = (TextView) rootView
+				.findViewById(R.id.txt_main_info_send_threshold);
 	}
 
 	private void setListeners() {
@@ -111,7 +120,8 @@ public class PayoutFragment extends Fragment {
 
 	private void setPrice(TextView txt, GenericPrice current) {
 		if (current != null) {
-			float lastPriceFloat = pref.getFloat("txt_" + txt.getId() + "_value", 0f);
+			float lastPriceFloat = pref.getFloat("txt_" + txt.getId()
+					+ "_value", 0f);
 			float currentPriceFloat = current.getValueFloat();
 
 			int minuteThreshold = App.getInstance().getPriceThreshold();
@@ -121,24 +131,37 @@ public class PayoutFragment extends Fragment {
 
 			long now = Calendar.getInstance().getTimeInMillis();
 
-			Log.d(getClass().getSimpleName(), "Pricethreshold min: " + minuteThreshold);
+			Log.d(getClass().getSimpleName(), "Pricethreshold min: "
+					+ minuteThreshold);
 			Log.d(getClass().getSimpleName(), "Pricethreshold: " + threshold);
-			Log.d(getClass().getSimpleName(), "Price last Updated: " + lastUpdated);
+			Log.d(getClass().getSimpleName(), "Price last Updated: "
+					+ lastUpdated);
 			Log.d(getClass().getSimpleName(), "Price now: " + now);
-			Log.d(getClass().getSimpleName(), "time until priceupdate: " + (((lastUpdated + threshold) - now) / 1000) + " sec");
+			Log.d(getClass().getSimpleName(), "time until priceupdate: "
+					+ (((lastUpdated + threshold) - now) / 1000) + " sec");
 
 			if ((lastUpdated + threshold) < now) {
 
-				Log.d(getClass().getSimpleName(), "threshold expired --> set colors for " + "txt_" + txt.getId());
+				Log.d(getClass().getSimpleName(),
+						"threshold expired --> set colors for " + "txt_"
+								+ txt.getId());
 				if (lastPriceFloat > currentPriceFloat) {
-					txtCurrentValue.setTextColor(getResources().getColor(R.color.bd_red));
+					txtCurrentValue.setTextColor(getResources().getColor(
+							R.color.bd_red));
 				} else {
-					txtCurrentValue.setTextColor(getResources().getColor(R.color.bd_green));
+					txtCurrentValue.setTextColor(getResources().getColor(
+							R.color.bd_green));
 				}
 
-				pref.edit().putFloat("txt_" + txt.getId() + "_value", currentPriceFloat).commit();
-				pref.edit().putLong("txt_" + txt.getId(), Calendar.getInstance().getTimeInMillis()).commit();
-				Log.d(getClass().getSimpleName(), "set last price to: " + currentPriceFloat);
+				pref.edit()
+						.putFloat("txt_" + txt.getId() + "_value",
+								currentPriceFloat).commit();
+				pref.edit()
+						.putLong("txt_" + txt.getId(),
+								Calendar.getInstance().getTimeInMillis())
+						.commit();
+				Log.d(getClass().getSimpleName(), "set last price to: "
+						+ currentPriceFloat);
 			}
 
 			txtCurrentValue.setText(App.formatPrice(current.getValueFloat()));
@@ -147,8 +170,8 @@ public class PayoutFragment extends Fragment {
 
 	private void setGauge(Profile profile) {
 		try {
-			int max = 1000;
-			int offset = 48;
+			int max = 100;
+			int offset = 4;
 
 			float sendThreshold = Float.valueOf(profile.getSend_threshold());
 			float estimated = Float.valueOf(profile.getEstimated_reward());
@@ -168,11 +191,19 @@ public class PayoutFragment extends Fragment {
 			confirmedProgress += offset;
 			total += offset;
 
+			if (confirmedProgress > max) {
+				confirmedProgress = max + offset;
+			}
+
+			if (total > max) {
+				total = max + offset;
+			}
+
 			prgGauge.setMax(max + (2 * offset));
-			prgGauge.setProgress(0);
-			prgGauge.setSecondaryProgress(0);
-			new TotalAnimator().execute(total);
-			new ConfirmedAnimator().execute(confirmedProgress);
+			
+			ProgressBarAnimation anim = new ProgressBarAnimation(prgGauge, offset, total, -50, confirmedProgress);
+			anim.setDuration(1000);
+			prgGauge.startAnimation(anim);
 
 		} catch (NullPointerException e) {
 			e.printStackTrace();
@@ -189,7 +220,7 @@ public class PayoutFragment extends Fragment {
 
 		@Override
 		protected void onProgressUpdate(Void... values) {
-			prgGauge.incrementProgressBy(10);
+			prgGauge.incrementProgressBy(1);
 		}
 
 		@Override
@@ -197,7 +228,7 @@ public class PayoutFragment extends Fragment {
 			while (prgGauge.getProgress() < params[0]) {
 				publishProgress();
 				try {
-					Thread.sleep(7);
+					Thread.sleep(10);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -210,7 +241,7 @@ public class PayoutFragment extends Fragment {
 
 		@Override
 		protected void onProgressUpdate(Void... values) {
-			prgGauge.incrementSecondaryProgressBy(10);
+			prgGauge.incrementSecondaryProgressBy(1);
 		}
 
 		@Override
@@ -218,13 +249,40 @@ public class PayoutFragment extends Fragment {
 			while (prgGauge.getSecondaryProgress() < params[0]) {
 				publishProgress();
 				try {
-					Thread.sleep(7);
+					Thread.sleep(10);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
 			return null;
 		}
+	}
+	
+	public class ProgressBarAnimation extends Animation{
+	    private ProgressBar progressBar;
+	    private float from;
+	    private float  to;
+	    private float secondFrom;
+	    private float secondTo;
+
+	    public ProgressBarAnimation(ProgressBar progressBar, float from, float to, float secondFrom, float secondTo) {
+	        super();
+	        this.progressBar = progressBar;
+	        this.from = from;
+	        this.to = to;
+	        this.secondFrom = secondFrom;
+	        this.secondTo = secondTo;
+	    }
+
+	    @Override
+	    protected void applyTransformation(float interpolatedTime, Transformation t) {
+	        super.applyTransformation(interpolatedTime, t);
+	        float value = from + (to - from) * interpolatedTime;
+	        float secondValue = secondFrom + (secondTo - secondFrom) * interpolatedTime;
+	        progressBar.setProgress((int) value);
+	        progressBar.setSecondaryProgress((int) secondValue);
+	    }
+
 	}
 
 }
