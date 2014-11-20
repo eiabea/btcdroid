09e66a832bf48353bb4937dd238146b240c9c333
eiabea <developer@eiabea.com>
2014-11-20 12:36:47 +0100

    got rid of libs

--- android/src/main/java/com/eiabea/btcdroid/fragments/PrefsFragment.java
+++ android/src/main/java/com/eiabea/btcdroid/fragments/PrefsFragment.java
@@ -5,10 +5,10 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.support.v4.preference.PreferenceFragment;
 
 import com.eiabea.btcdroid.PrefsActivity;
 import com.eiabea.btcdroid.R;
+import com.github.machinarius.preferencefragment.PreferenceFragment;
 
 import java.util.ArrayList;
 import java.util.List;
