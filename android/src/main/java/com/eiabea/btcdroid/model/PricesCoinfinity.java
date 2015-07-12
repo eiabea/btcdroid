ac850f2dd96c0a66d84e1860337c6739a0eeee9e
eiabea <developer@eiabea.com>
2015-07-12 11:19:50 +0200

    strings to float version bump

--- android/src/main/java/com/eiabea/btcdroid/model/PricesCoinfinity.java
+++ android/src/main/java/com/eiabea/btcdroid/model/PricesCoinfinity.java
@@ -1,44 +1,26 @@
 package com.eiabea.btcdroid.model;
 
-import com.eiabea.btcdroid.model.btce.BTCeTicker;
-
 public class PricesCoinfinity {
 
     // Attributes
-    private BTCeTicker ticker;
-    private String base;
-    private String atm;
-    private String bitcoinbon;
-
-//    {"base":"254.33","atm":"263.85","bitcoinbon":"275.06","pair":"XBTEUR","timestamp":1436534288}
-
+    private float base;
+    private float atm;
+    private float bitcoinbon;
 
     // Standardconstructor
     public PricesCoinfinity() {
     }
 
     public float getBase() {
-        try {
-            return Float.parseFloat(this.base);
-        } catch (NumberFormatException ex) {
-            return 0;
-        }
+        return base;
     }
 
     public float getAtm() {
-        try {
-            return Float.parseFloat(this.atm);
-        } catch (NumberFormatException ex) {
-            return 0;
-        }
+        return atm;
     }
 
     public float getBitcoinbon() {
-        try {
-            return Float.parseFloat(this.bitcoinbon);
-        } catch (NumberFormatException ex) {
-            return 0;
-        }
+        return bitcoinbon;
     }
 
 }
